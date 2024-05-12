ce036776e9aa455fce45ebcdc932d0f35ce898d7
Javier <106848262+javiiicz@users.noreply.github.com>
2024-05-12 15:45:21 -0600

    Header implementation

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <header class="header">
+            <nav class="navstyle">
+                <div id="logoID" class="logocontainer">
+                    <div class="logostyle">Javier Carrillo</div>
+                </div>
+                <div class="linkcontainer">
+                    <a href="#" class="navlink">Link 1</a>
+                    <a href="#" class="navlink">Link2</a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
