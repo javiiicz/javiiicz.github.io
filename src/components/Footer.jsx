830d70995bb65c9df96740948e0803ad95b5acc8
Javier <106848262+javiiicz@users.noreply.github.com>
2024-12-18 15:49:35 -0600

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,28 @@
 function Footer() {
     return (
-        <footer className="static bg-[url('/src/images/Footer-background-2.png')] bg-cover bg-bottom">
-            <div className="noise-overlay backdrop-blur-2xl border-t border-gray-200 flex flex-row-reverse justify-between h-40 md:px-72 sm:px-32 px-10">
-                <div className="text-4xl font-extrabold grid grid-rows-2 gap-0 items-center justify-items-center">
-                    <p className="drop-shadow-lg">Socials</p>
-                    <div className="grid grid-cols-2 gap-3">
-                        <a
-                        href="https://www.github.com/javiiicz"
-                        className="w-12 h-12 bg-[url('/src/images/GitHub-Logo.svg')] bg-cover"
-                        > </a>
-                        <a
-                        href="https://www.linkedin.com/in/jcarrilloz/"
-                        className="w-12 h-12 bg-[url('/src/images/LinkedIn-Logo.svg')] bg-cover"
-                        > </a>
+        <div>
+            <p className="pr-5 italic text-gray-500 text-right">Last Updated Dec 2024</p>
+            <footer className="static bg-[url('/src/images/Footer-background-2.png')] bg-cover bg-bottom">
+                <div className="noise-overlay backdrop-blur-2xl border-t border-gray-200 flex flex-row-reverse justify-between h-40 md:px-72 sm:px-32 px-10">
+                    <div className="text-4xl font-extrabold grid grid-rows-2 gap-0 items-center justify-items-center">
+                        <p className="drop-shadow-lg">Socials</p>
+                        <div className="grid grid-cols-2 gap-3">
+                            <a
+                            href="https://www.github.com/javiiicz"
+                            className="w-12 h-12 bg-[url('/src/images/GitHub-Logo.svg')] bg-cover"
+                            > </a>
+                            <a
+                            href="https://www.linkedin.com/in/jcarrilloz/"
+                            className="w-12 h-12 bg-[url('/src/images/LinkedIn-Logo.svg')] bg-cover"
+                            > </a>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1 gap-5 items-center justify-items-center">
+                        <p>© 2024 Javier Carrillo</p>
                     </div>
                 </div>
-                <div className="grid grid-cols-1 gap-5 items-center justify-items-center">
-                    <p>© 2024 Javier Carrillo</p>
-                </div>
-            </div>
-        </footer>
+            </footer>
+        </div>
 
     )
 }
