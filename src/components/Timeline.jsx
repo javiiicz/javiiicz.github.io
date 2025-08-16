da2f8075b8940165e5d69e446ac20709e1ec6514
Javier <106848262+javiiicz@users.noreply.github.com>
2025-08-15 21:04:31 -0400

    [Info] Timeline Update

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,4 +1,4 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import UMUrl from "../images/UMLogo.png"
 import KTPUrl from "../images/ktp_logo.jpeg"
@@ -13,109 +13,112 @@ import tealBlobUrl from "../images/TealBlob.svg"
 function Timeline() {
     return (
         <section id="Experience">
-        <div className="relative">
-            <img src={greenBlobUrl} loading="lazy" alt="" className="absolute w-[400px] top-[300px] right-[100px]" />
-            <img src={redBlobUrl} loading="lazy" alt="" className="absolute w-[300px] -top-[100px] left-[0px]" />
-            <img src={blueBlobUrl} loading="lazy" alt="" className="absolute w-[400px] -bottom-[100px] -left-[100px]" />
-            <img src={tealBlobUrl} loading="lazy" alt="" className="absolute w-[500px] top-[50px] -right-[200px]" />
+            <div className="relative">
+                <img src={greenBlobUrl} loading="lazy" alt="" className="absolute w-[400px] top-[300px] right-[100px]" />
+                <img src={redBlobUrl} loading="lazy" alt="" className="absolute w-[300px] -top-[100px] left-[0px]" />
+                <img src={blueBlobUrl} loading="lazy" alt="" className="absolute w-[400px] -bottom-[100px] -left-[100px]" />
+                <img src={tealBlobUrl} loading="lazy" alt="" className="absolute w-[500px] top-[50px] -right-[200px]" />
 
 
-            <div className="noise-overlay backdrop-blur bg-gray-100/50 px-[10%] shadow-glass py-6">
-                <h2 className="px-0 pb-1 font-bold text-3xl drop-shadow" data-aos="fade-right"> Experience </h2>
-                <div className="p-5" >
-                <VerticalTimeline
-                    lineColor={'#111827'}>
-                    
-                    <VerticalTimelineElement
-                        icon={ <UMIcon /> }
-                        iconClassName='overflow-hidden bg-gray-50 object-cover'
-                        iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
-                        date='Aug 2023 - Present'
-                    >
-                        <h3 className="my-0 text-lg">University of Miami</h3>
-                        <div className="text-sm italic">
-                            B.S Computer Science &amp; Mathematics
-                        </div>
-                        <p className="mt-2">
-                            I'm currently pursuing a double major with concentration in Data Science
-                            and Statistics, respectively.
-                        </p>
-                    </VerticalTimelineElement>
-                    
-                    <VerticalTimelineElement
-                        icon={ <KTPIcon /> }
-                        iconClassName='overflow-hidden border-gray-400'
-                        iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
-                        date='April 2024 - Present'
+                <div className="noise-overlay backdrop-blur bg-gray-100/50 px-[10%] shadow-glass py-6">
+                    <h2 className="px-0 pb-1 font-bold text-3xl drop-shadow" data-aos="fade-right"> Experience </h2>
+                    <div className="p-5" >
+                        <VerticalTimeline
+                            lineColor={'#111827'}>
 
-                    >
-                        <h3 className="my-0 text-lg">
-                            Kappa Theta Pi - Professional Fraternity
-                        </h3>
-                        <div className="text-sm italic">
-                            Director of Technical Operations
-                        </div>
-                        <p className="mt-2">
-                            I update and maintain communication channels, including the website,
-                            with the most recent information regarding members and events.
-                        </p>
-                    </VerticalTimelineElement>
-                    
-                    <VerticalTimelineElement
-                        icon={ <BonsaiIcon /> }
-                        iconClassName='overflow-hidden bg-gray-50 border-gray-400'
-                        iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
-                        date='September 2024 - Present'
+                            <VerticalTimelineElement
+                                icon={<UMIcon />}
+                                iconClassName='overflow-hidden bg-gray-50 object-cover'
+                                iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
+                                date='Aug 2023 - Present'
+                            >
+                                <h3 className="my-0 text-lg">University of Miami</h3>
+                                <div className="text-sm italic">
+                                    B.S Computer Science &amp; Mathematics
+                                </div>
+                                <p className="mt-2">
+                                    I'm currently pursuing a double major with concentration in Data Science
+                                    and Statistics, respectively.
+                                </p>
+                            </VerticalTimelineElement>
 
-                    >
-                        <h3 className="my-0 text-lg">
-                            Bonsai Applied Computations Group
-                        </h3>
-                        <div className="text-sm italic">
-                            Research Analyst
-                        </div>
-                        <p className="mt-2">
-                        I develop and deploy computer science applications to support faculty research, boosting interdisciplinary research
-                        efforts and enhancing academic output across various fields.
-                        </p>
-                    </VerticalTimelineElement>
+                            <VerticalTimelineElement
+                                icon={<KTPIcon />}
+                                iconClassName='overflow-hidden border-gray-400'
+                                iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
+                                date='April 2024 - August 2025'
 
-                    <VerticalTimelineElement
-                        icon={ <IDSCIcon /> }
-                        iconClassName='overflow-hidden bg-gray-50 border-gray-400'
-                        iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
-                        date='January 2025 - Present'
+                            >
+                                <h3 className="my-0 text-lg">
+                                    Kappa Theta Pi - Professional Fraternity
+                                </h3>
+                                <div className="text-sm italic">
+                                    Director of Technical Operations
+                                </div>
+                                <p className="mt-2">
+                                    I updated and maintained communication channels, including the website,
+                                    with the most recent information regarding members and events.
+                                </p>
+                            </VerticalTimelineElement>
 
-                    >
-                        <h3 className="my-0 text-lg">
-                            University of Miami Frost Institute for Data Science & Computing
-                        </h3>
-                        <div className="text-sm italic">
-                            XR Developer
-                        </div>
-                        <p className="mt-2">
-                        I am an XR developer at the University of Miami, where I work on cutting-edge immersive media projects with students and faculty. 
-                        </p>
-                    </VerticalTimelineElement>
+                            <VerticalTimelineElement
+                                icon={<BonsaiIcon />}
+                                iconClassName='overflow-hidden bg-gray-50 border-gray-400'
+                                iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
+                                date='September 2024 - Present'
 
-                    <VerticalTimelineElement
-                        icon={ <MetaIcon /> }
-                        iconClassName='overflow-hidden bg-gray-50 border-gray-400'
-                        iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
-                        date='June 2025'
+                            >
+                                <h3 className="my-0 text-lg">
+                                    Bonsai Applied Computations Group
+                                </h3>
+                                <div className="text-sm italic">
+                                    Research Analyst
+                                </div>
+                                <p className="mt-2">
+                                    I develop and deploy computer science applications to support faculty research, boosting interdisciplinary research
+                                    efforts and enhancing academic output across various fields.
+                                </p>
+                            </VerticalTimelineElement>
 
-                    >
-                        <h3 className="my-0 text-lg">
-                            Meta
-                        </h3>
-                        <div className="text-sm italic">
-                            Incoming Meta University Software Engineer
-                        </div>
-                    </VerticalTimelineElement>
-                </VerticalTimeline>
+                            <VerticalTimelineElement
+                                icon={<IDSCIcon />}
+                                iconClassName='overflow-hidden bg-gray-50 border-gray-400'
+                                iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
+                                date='January 2025 - Present'
+
+                            >
+                                <h3 className="my-0 text-lg">
+                                    University of Miami Frost Institute for Data Science & Computing
+                                </h3>
+                                <div className="text-sm italic">
+                                    XR Developer
+                                </div>
+                                <p className="mt-2">
+                                    I am an XR developer at the University of Miami, where I work on cutting-edge immersive media projects with students and faculty.
+                                </p>
+                            </VerticalTimelineElement>
+
+                            <VerticalTimelineElement
+                                icon={<MetaIcon />}
+                                iconClassName='overflow-hidden bg-gray-50 border-gray-400'
+                                iconStyle={{ boxShadow: '#111827 0px 0px 0px 4px' }}
+                                date='June 2025 - August 2025'
+
+                            >
+                                <h3 className="my-0 text-lg">
+                                    Meta
+                                </h3>
+                                <div className="text-sm italic">
+                                    Software Engineer Intern
+                                </div>
+                                <p className="mt-2">
+                                    I developed a full-stack application using React, Node, Postgres, and other APIs with the help of a manager and peers.
+                                </p>
+                            </VerticalTimelineElement>
+                        </VerticalTimeline>
+                    </div>
                 </div>
             </div>
-        </div>
         </ section>
     )
 }
@@ -155,7 +158,7 @@ function BonsaiIcon() {
 function MetaIcon() {
     return (
         <div>
-            <img src={MetaUrl} loading="lazy" alt="Meta Logo"/>
+            <img src={MetaUrl} loading="lazy" alt="Meta Logo" />
         </div>
     )
 }
